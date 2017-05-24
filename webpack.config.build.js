162753700ad0fb7ef502ee61c94341202b66029f
Sander <sander@funkhaus.us>
2017-05-24 17:05:46 -0400

    Moving to web pack workflow

---
+++ webpack.config.build.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpackConfig = require('./webpack.config');
+
+module.exports = Object.assign(webpackConfig, {
+
+    devtool: 'cheap-module-source-map',
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+
+    plugins: webpackConfig.plugins.concat([
+
+        new CleanWebpackPlugin(['dist'])
+    ])
+
+});
