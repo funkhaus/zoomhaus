a3cdafb1e4b6e6e909f1a0194d68618f98745dd6
Sander Moolin <sandermoolin@gmail.com>
2017-10-24 17:04:40 -0400

    Trimming code, improving openOverlay comments

--- src/openOverlay.js
+++ src/openOverlay.js
@@ -2,6 +2,10 @@ import { setDefault, q, qa, createAndAppend } from './utils'
 
 export default ( target, settings, winDimensions ) => {
 
+    // User has clicked on "target", an element that they expect to zoom in and take over the screen.
+    // We're going to clone the target, place it directly above the original, and animate that clone to the center of the screen
+    // while expanding its width/height to the maximum allowed sizes.
+
     // abort if there is already an instance open or if we're animating
     if ( document.body.classList.contains('zoomhaus-open')
         || document.body.classList.contains('zoomhaus-transitioning') )
@@ -11,31 +15,32 @@ export default ( target, settings, winDimensions ) => {
     target.classList.add('active')
 
     // get rectangle for image as it sits in the page
-    var imgRect = target.getBoundingClientRect()
+    var targetRect = target.getBoundingClientRect()
 
     // build styling
     let css = {
-        '-webkit-transform': `-webkit-translate(${imgRect.left}px, ${imgRect.top}px)`,
-        transform: `translate(${imgRect.left}px, ${imgRect.top}px)`,
+        '-webkit-transform': `-webkit-translate(${targetRect.left}px, ${targetRect.top}px)`,
+        transform: `translate(${targetRect.left}px, ${targetRect.top}px)`,
         position: 'absolute',
-        width: `${imgRect.width}px`,
+        width: `${targetRect.width}px`,
         height: 'auto',
     }
 
     // determine if we need any clipping
     if( settings.grow ){
-        // Calculate the height difference between this and parent container
+        // Calculate the height difference between the target and parent container
         var parentHeight = target.parentNode.getBoundingClientRect().height;
         var diff = target.getBoundingClientRect().height - parentHeight;
         css['-webkit-clip-path'] = '-webkit-inset(' + (diff / 2) + 'px 0)';
         css['clip-path'] = 'inset(' + (diff / 2) + 'px 0)';
     }
 
-    // clone target image, position it
+    // clone target image and remove unnecessary classes
     const newImg = target.cloneNode()
     newImg.classList.remove('active')
     newImg.classList.remove('zoomhaus-target')
 
+    // apply styling
     for( let prop in css ){
         newImg.style[prop] = css[prop]
     }
@@ -52,23 +57,22 @@ export default ( target, settings, winDimensions ) => {
     const width = target.getAttribute('width') || target.getBoundingClientRect().width
     const naturalRatio = height / width
 
-    // get target dimensions
+    // get optimal dimensions
     let targetHeight = Math.min( (window.innerHeight - settings.marginY * 2), height )
     let targetWidth = Math.min( (window.innerWidth - settings.marginX * 2), width )
 
-    // if fitting to width is too tall...
+    // if fitting to width makes the clone too tall...
     if ( targetWidth * naturalRatio > targetHeight ){
         // fit to height
-        targetWidth = targetHeight / naturalRatio;
-    // fitting to width was NOT too small...
+        targetWidth = targetHeight / naturalRatio
     } else {
         // fit to width
-        targetHeight = targetWidth * naturalRatio;
+        targetHeight = targetWidth * naturalRatio
     }
 
     // calculate transform properties
-    const scale = targetWidth / width;
-    const antiScale = width / targetWidth;
+    // const scale = targetWidth / width
+    // const antiScale = width / targetWidth
 
     // add body classes
     document.body.classList.add('zoomhaus-open')
